f141f88c667cf33a639f310df310b2b9bd1e4c53
Rumesh Eranga <rehrumesh@hotmail.com>
2015-08-11 18:29:21 +0530

    Basic Mirf Configurations added. Incomplete though

--- MotherMote/MotherMote.ino
+++ MotherMote/MotherMote.ino
@@ -1,7 +1,64 @@
+/*
+nRF24L01 to Arduino Uno
+MISO -> 12
+MOSI -> 11
+SCK -> 13
+CE -> 8
+CSN -> 7
+GND -> GND
+VCC -> 3V3
+*/
+
+
+#include <SPI.h>
+#include <Mirf.h>
+#include <nRF24L01.h>
+#include <MirfHardwareSpiDriver.h>
+
+#define MAX_NODE_LIMIT 25
+#define MOTHERMOTE_ID 1
+
+static byte sensors[MAX_NODE_LIMIT]; 
+
+struct init_request{
+	byte sensornode_id;	// if sensornode_id > MAX_NODE_LIMIT it is a new node
+};
+
+struct init_response{
+	byte mothermote_id;
+	byte sensornode_id;
+	int wakeup_delay;
+};
+
 void setup(){
-  
+	Serial.begin(9600);
+	Mirf.spi = &MirfHardwareSpi;
+	Mirf.init();
+	Mirf.setRADDR((byte *) "cross");
+	Mirf.channel = 50;
+	//Mirf.payload = sizeof(compressed_packet);              
+	Mirf.config();
+
 }
 
 void loop(){
 
+	static init_request request;
+	static init_response response;
+
+	while(!Mirf.dataReady());
+
+	if(Mirf.dataReady()){
+		Mirf.getData((byte *) &request);
+		if(request.sensornode_id > MAX_NODE_LIMIT){
+			response.mothermote_id = MOTHERMOTE_ID;
+			response.sensornode_id = generateSensorNodeId();
+			
+		}
+	}
+
+}
+
+byte generateSensorNodeId(){
+	return 50;
 }
--- SensorNode/SensorNode.ino
+++ SensorNode/SensorNode.ino
@@ -1,7 +1,38 @@
+#include <SPI85.h>
+#include <Mirf.h>
+#include <nRF24L01.h>
+#include <MirfHardwareSpi85Driver.h>
+
+#define MAX_NODE_ID 25
+
+struct init_request{
+	byte sensornode_id;	// if sensornode_id > MAX_NODE_ID it is a new node
+};
+
+struct init_response{
+	byte mothermote_id;
+	byte sensornode_id;
+	int wakeup_delay;
+};
+
+
+static byte sensornode_id;
+
+
 void setup(){
-  
+	sensornode_id = random(MAX_NODE_ID) + MAX_NODE_ID;
+	Mirf.cePin = PB4;
+	Mirf.csnPin = PB3;
+
+	Mirf.spi = &MirfHardwareSpi85;	  
+	Mirf.init();
+	Mirf.setRADDR((byte *) "cross");
+	Mirf.channel = 50;
+	Mirf.config();
 }
 
 void loop(){
 
 }
+
+
